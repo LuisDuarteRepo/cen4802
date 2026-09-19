c2e9313f23701fe79bc93437ce6503143d759b60
LuisDuarteRepo <lduarte4@mail.valenciacollege.edu>
2026-09-18 23:28:52 -0400

    Made a login dialog and changed the jdk version of the pom.xml

--- cen4802-project-module/src/main/java/com/cen4802Project/app/MainView.java
+++ cen4802-project-module/src/main/java/com/cen4802Project/app/MainView.java
@@ -3,6 +3,7 @@ package com.cen4802Project.app;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -18,8 +19,6 @@ import java.util.List;
 @Route("")
 public class MainView extends VerticalLayout {
     private UserRepository repository;
-    private TextField username = new TextField("username");
-    private PasswordField password = new PasswordField("password");
     private TextField firstName = new TextField("First Name");
     private TextField lastName = new TextField("Last Name");
     private EmailField email = new EmailField("Email");
@@ -41,10 +40,10 @@ public class MainView extends VerticalLayout {
         var layout = new HorizontalLayout();
         layout.setAlignItems(Alignment.BASELINE);
 
-        var addButton = new Button("Add");
-        var loginButton = new Button("Login");
+        Button addButton = new Button("Add");
+        Button loginButton = new Button("Login");
         addButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        layout.add(username, password, firstName, lastName, email, addButton);
+        layout.add(firstName, lastName, email, addButton, loginButton);
 
         //inspects layout for fields that match the fields in the model and binds them together
         binder.bindInstanceFields(this);
@@ -61,14 +60,9 @@ public class MainView extends VerticalLayout {
             }
         });
 
-//        loginButton.addClickListener(click -> {
-//            try {
-//                var login = new User();
-//                loginButton.setText("Successfully logged in!");
-//            } catch (Exception e) {
-//                //
-//            }
-//        });
+        loginButton.addClickListener(c ->{
+           openLoginDialog();
+        });
 
         return layout;
     }
@@ -76,5 +70,32 @@ public class MainView extends VerticalLayout {
     private void refreshGrid() {
         List<User> users = repository.findAll();
         grid.setItems(users);
+    }
+
+    String username = "";
+    String password = "";
+
+    public void openLoginDialog(){
+        Dialog dialog = new Dialog();
+        TextField usernameField = new TextField("username");
+        PasswordField passwordField = new PasswordField("password");
+
+        usernameField.addValueChangeListener(e -> {
+            username = e.getValue();
+            password = e.getValue();
+        });
+        Button closeButton = new Button("Close", e -> {
+            dialog.close();
+        });
+        Button loginButton = new Button("Login",e ->{
+            //
+        });
+        dialog.add(usernameField, passwordField, loginButton, closeButton);
+        dialog.addOpenedChangeListener(e ->{
+            if(!e.isOpened()){
+                System.out.println("Values entered:" + username + password);
+            }
+        });
+        dialog.open();
     }
 }
